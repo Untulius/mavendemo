62239d35f0c936a99b850a9cd315dbba0f556b78
Victor Untulius <untulius@gmail.com>
2020-03-14 19:18:16 +0300

    Задание 12

--- src/main/java/com/github/gittest/DoAnything.java
+++ src/main/java/com/github/gittest/DoAnything.java
@@ -3,11 +3,23 @@ package com.github.gittest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*Создать мавен-проект, добавить в pom.xml зависимость slf4j-log4j12,
+добавить конфигурационный файл для логера (log4j.properties). Вывести
+в консоль и в файл сообщения уровня ERROR, WARN, INFO, TRACE*/
+
 public class DoAnything {
     private static final Logger LOG = LoggerFactory.getLogger(DoAnything.class);
 
-    public void greeting(String name){
-        LOG.info("Привет, {}!", name);
+    public void greeting(){
+        LOG.error("Сообщение уровня ERROR");
+        LOG.warn("Сообщение уровня WARN");
+        LOG.info("Сообщение уровня INFO");
+        LOG.trace("Сообщение уровня TRACE");
+    }
+
+    public static void main(String[] args) {
+        DoAnything doAnything = new DoAnything();
+        doAnything.greeting();
     }
 
 }
